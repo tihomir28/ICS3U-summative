5ffe95cd2dd6a4ab81740e66883e5c6947365bd0
stalex28 <adjor1@ocdsb.ca>
2019-01-16 20:39:08 -0500

    See description
    -Fixed bug where a win would be declared falsely
    -implemented start screen
    -improved win screen

--- connect4.h
+++ connect4.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_native_dialog.h>
@@ -17,12 +18,14 @@ ALLEGRO_BITMAP *image = nullptr;
 ALLEGRO_EVENT_QUEUE *event_queue;
 ALLEGRO_EVENT ev;
 ALLEGRO_TIMER *timer;
+ALLEGRO_FONT *arial;
 #define WHITE al_map_rgb(255,255,255)
 #define RED al_map_rgb(255,0,0)
 #define BLACK al_map_rgb(0,0,0)
+#define BLUE al_map_rgb(80, 80, 250)
 
 int initializeAllegro(const char title[]);
-int drawScreen(int f[][6], int verticalRow, int hue);
+void drawScreen(int f[][6], int verticalRow, int hue, int modifier);
 bool checkerplacer(int f[][6], int column, int colour);
-int column(coord xyvalues);
+int column(coord xyvalues, int screen);
 bool winner(int board[][6], coord lastPlaced, int tint);
--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main(int argc, char *argv[]) {
 	int field[7][6] = {0};
 	bool exit = false;
 	int colour = 1;
+	int switcher = 0;
 	coord mouseCoordinates;
-	drawScreen(field, 7, 0);
+	drawScreen(field, 7, 0, switcher);
 
 	al_start_timer(timer);
     while(!exit){
@@ -23,8 +24,12 @@ int main(int argc, char *argv[]) {
         else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){
             mouseCoordinates.mx = ev.mouse.x;
             mouseCoordinates.my = ev.mouse.y;
-            if(field[column(mouseCoordinates)][0] == 0){
-                exit = checkerplacer(field, column(mouseCoordinates), colour);
+            if(switcher == 0){
+                if(column(mouseCoordinates, switcher) == 7)
+                    switcher = 1;
+            }
+            else if(field[column(mouseCoordinates, switcher)][0] == 0){
+                exit = checkerplacer(field, column(mouseCoordinates, switcher), colour);
                 if(colour == 1)
                     colour = 2;
                 else
@@ -32,7 +37,7 @@ int main(int argc, char *argv[]) {
             }
         }
         else if(ev.type == ALLEGRO_EVENT_TIMER){
-            drawScreen(field, column(mouseCoordinates), colour);
+            drawScreen(field, column(mouseCoordinates, switcher), colour, switcher);
         }
     }
     return 0;
@@ -49,11 +54,11 @@ bool checkerplacer(int f[][6], int column, int colour){
         }
     }
     if(!winner(f, placedPiece, colour)){
-        drawScreen(f, 7, 0);
+        drawScreen(f, 7, 0, 1);
         return false;
     }
     else{
-        drawScreen(f, 8, colour);
+        drawScreen(f, 8, colour, 1);
         return true;
     }
 }
@@ -65,15 +70,15 @@ bool winner(int board[][6], coord lastPlaced, int tint){
     for(int i = 1; i < 4; i++){
         if(lastPlaced.mx - i <= -1)
             stopper[0] == true;
-        if(lastPlaced.mx + i >= 7)
-            stopper[1] == true;
-        if(!stopper[0]){
+        else if(!stopper[0]){
             if(board[lastPlaced.mx-i][lastPlaced.my] == tint)
                 counter ++;
             else
                 stopper[0] = true;
         }
-        if(!stopper[1]){
+        if(lastPlaced.mx + i >= 7)
+            stopper[1] == true;
+        else if(!stopper[1]){
             if(board[lastPlaced.mx+i][lastPlaced.my] == tint)
                 counter ++;
             else
@@ -89,7 +94,7 @@ bool winner(int board[][6], coord lastPlaced, int tint){
     for(int i = 1; i < 4; i++){
         if(lastPlaced.my + i >= 6)
             stopper[1] == true;
-        if(!stopper[1]){
+        else if(!stopper[1]){
             if(board[lastPlaced.mx][lastPlaced.my+i] == tint)
                 counter ++;
             else
@@ -103,21 +108,27 @@ bool winner(int board[][6], coord lastPlaced, int tint){
     stopper[1] = false;
     /// direction 3: first diagonal (\)
     for(int i = 1; i < 4; i++){
-        if(lastPlaced.mx - i <= -1 || lastPlaced.my - i <= -1)
+        if(lastPlaced.mx - i <= -1 || lastPlaced.my - i <= -1){
             stopper[0] == true;
-        if(lastPlaced.mx + i >= 7 || lastPlaced.my + i >= 6)
-            stopper[1] == true;
-        if(!stopper[0]){
-            if(board[lastPlaced.mx-i][lastPlaced.my-i] == tint)
-                counter ++;
-            else
-                stopper[0] = true;
         }
-        if(!stopper[1]){
-            if(board[lastPlaced.mx+i][lastPlaced.my+i] == tint)
+        else if(!stopper[0]){
+            if(board[lastPlaced.mx-i][lastPlaced.my-i] == tint){
                 counter ++;
-            else
+            }
+            else{
+                stopper[0] = true;
+            }
+        }
+        if(lastPlaced.mx + i >= 7 || lastPlaced.my + i >= 6){
+            stopper[1] == true;
+        }
+        else if(!stopper[1]){
+            if(board[lastPlaced.mx+i][lastPlaced.my+i] == tint){
+                counter ++;
+            }
+            else{
                 stopper[1] = true;
+            }
         }
         if(counter >= 3)
             return true;
@@ -129,15 +140,15 @@ bool winner(int board[][6], coord lastPlaced, int tint){
     for(int i = 1; i < 4; i++){
         if(lastPlaced.mx - i <= -1 || lastPlaced.my + i >= 6)
             stopper[0] == true;
-        if(lastPlaced.mx + i >= 7 || lastPlaced.my - i <= -1)
-            stopper[1] == true;
-        if(!stopper[0]){
+        else if(!stopper[0]){
             if(board[lastPlaced.mx-i][lastPlaced.my+i] == tint)
                 counter ++;
             else
                 stopper[0] = true;
         }
-        if(!stopper[1]){
+        if(lastPlaced.mx + i >= 7 || lastPlaced.my - i <= -1)
+            stopper[1] == true;
+        else if(!stopper[1]){
             if(board[lastPlaced.mx+i][lastPlaced.my-i] == tint)
                 counter ++;
             else
@@ -149,59 +160,84 @@ bool winner(int board[][6], coord lastPlaced, int tint){
     return false;
 }
 
-int column(coord xyvalues){
-    if((xyvalues.mx > 111 && xyvalues.mx < 191) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 0;
-    else if((xyvalues.mx > 191 && xyvalues.mx < 248) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 1;
-    else if((xyvalues.mx > 248 && xyvalues.mx < 304) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 2;
-    else if((xyvalues.mx > 304 && xyvalues.mx < 358) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 3;
-    else if((xyvalues.mx > 358 && xyvalues.mx < 414) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 4;
-    else if((xyvalues.mx > 414 && xyvalues.mx < 469) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 5;
-    else if((xyvalues.mx > 469 && xyvalues.mx < 555) && (xyvalues.my > 54 && xyvalues.my < 350))
-        return 6;
+int column(coord xyvalues, int screen){
+    if(screen == 1){
+        if((xyvalues.mx > 111 && xyvalues.mx < 191) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 0;
+        else if((xyvalues.mx > 191 && xyvalues.mx < 248) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 1;
+        else if((xyvalues.mx > 248 && xyvalues.mx < 304) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 2;
+        else if((xyvalues.mx > 304 && xyvalues.mx < 358) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 3;
+        else if((xyvalues.mx > 358 && xyvalues.mx < 414) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 4;
+        else if((xyvalues.mx > 414 && xyvalues.mx < 469) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 5;
+        else if((xyvalues.mx > 469 && xyvalues.mx < 555) && (xyvalues.my > 54 && xyvalues.my < 350))
+            return 6;
+    }
+    else if(screen == 0){
+        if((xyvalues.mx > 280 && xyvalues.mx < 380) && (xyvalues.my > 370 && xyvalues.my < 410))
+            return 7;
+    }
 }
 
-int drawScreen(int f[][6], int verticalRow, int hue){
-    image = al_load_bitmap("c4.bmp");
-    al_draw_bitmap(image, 0,  0, 0);
-    for(int i=0; i<=5; i++){
-        for(int j=0; j<=6; j++){
-            switch(f[j][i]){
-                case 0:
-                    al_draw_filled_circle(165+(j*55), 80+(i*50), 23, WHITE);
-                    break;
-                case 1:
-                    al_draw_filled_circle(165+(j*55), 80+(i*50), 23, RED);
-                    break;
-                case 2:
-                    al_draw_filled_circle(165+(j*55), 80+(i*50), 23, BLACK);
-                    break;
+void drawScreen(int f[][6], int verticalRow, int hue, int modifier){
+    if(modifier == 1){
+        image = al_load_bitmap("c4.bmp");
+        al_draw_bitmap(image, 0,  0, 0);
+        for(int i=0; i<=5; i++){
+            for(int j=0; j<=6; j++){
+                switch(f[j][i]){
+                    case 0:
+                        al_draw_filled_circle(165+(j*55), 80+(i*50), 23, WHITE);
+                        break;
+                    case 1:
+                        al_draw_filled_circle(165+(j*55), 80+(i*50), 23, RED);
+                        break;
+                    case 2:
+                        al_draw_filled_circle(165+(j*55), 80+(i*50), 23, BLACK);
+                        break;
+                }
+            }
+        }
+        if(verticalRow < 7){
+            if(hue == 1)
+                al_draw_filled_circle(165+(verticalRow*55), 30, 23, RED);
+            else
+                al_draw_filled_circle(165+(verticalRow*55), 30, 23, BLACK);
+        }
+        if(verticalRow == 8){
+            if(hue == 1){
+                al_draw_text(arial, BLACK, 255, 10, 0, "RED WINS!");
+            }
+            else{
+                al_draw_text(arial, BLACK, 255, 10, 0, "BLACK WINS!");
             }
         }
     }
-    if(verticalRow < 7){
-        if(hue == 1)
-            al_draw_filled_circle(165+(verticalRow*55), 30, 23, RED);
-        else
-            al_draw_filled_circle(165+(verticalRow*55), 30, 23, BLACK);
-    }
-    if(verticalRow == 8){
-        if(hue == 1){
-            al_draw_filled_circle(23, 23, 23, RED);
+    if(modifier == 0){
+        al_clear_to_color(WHITE);
+        al_draw_bitmap(image, 0, 0, 0);
+        for(int i=0; i<=5; i++){
+            for(int j=0; j<=6; j++){
+                if((i+j)%2 == 1){
+                    al_draw_filled_circle(165+(j*55), 80+(i*50), 23, RED);
+                }
+                else{
+                    al_draw_filled_circle(165+(j*55), 80+(i*50), 23, BLACK);
+                }
+            }
         }
-        else{
-            al_draw_filled_circle(23, 23, 23, BLACK);
-        }
+        al_draw_text(arial, BLACK, 255, 10, 0, "Connect 4");
+        al_draw_filled_rectangle(280, 370, 380, 410, BLUE);
+        al_draw_text(arial, BLACK, 300, 370, 0, "Play");
+        al_flip_display();
     }
     al_flip_display();
     if(verticalRow == 8)
         al_rest(5);
-    return 0;
 }
 
 
@@ -238,16 +274,25 @@ int initializeAllegro(const char title[]){
                                  nullptr, ALLEGRO_MESSAGEBOX_ERROR);
       	al_destroy_display(display);
      	return -1;
-	 }
-	 if (!al_init_primitives_addon()) {
-    	al_show_native_message_box(display, "Error", "Error", "Failed to initialize primatives addon!",
+    }
+    if (!al_init_primitives_addon()) {
+        al_show_native_message_box(display, "Error", "Error", "Failed to initialize primatives addon!",
                                  nullptr, ALLEGRO_MESSAGEBOX_ERROR);
     	return -1;
 	}
 	if(!al_install_mouse()) {
       fprintf(stderr, "failed to initialize the mouse!\n");
       return -1;
-   }
+    }
+    al_init_font_addon(); // initialize the font addon
+    al_init_ttf_addon();// initialize the ttf (True Type Font) addon
+
+    arial = al_load_ttf_font("arial.ttf", 32, 0);
+    if (!arial){
+        al_show_native_message_box(display, "Error", "Error", "Could not load arial.ttf",
+                        nullptr, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
 
 	event_queue = al_create_event_queue();
 	if (!event_queue) {
